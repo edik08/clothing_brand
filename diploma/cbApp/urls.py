e65904b2530bf42c7389353994a964e186bae7af
Эдуард <n3zox1@yandex.ru>
2023-04-28 19:23:43 +0300

    настроили проект

--- diploma/cbApp/urls.py
+++ diploma/cbApp/urls.py
@@ -1,5 +1,10 @@
 from cbApp import views
-from django.urls import path
+from django.urls import path, re_path
+
 urlpatterns = [
-    path('ALLProducts/', views.getALLProducts)
+    path('ALLProducts/', views.getALLProducts),
+    path('Products/create', views.add_product),
+    re_path(r'Product/$', views.get_Product_by_id),
+    path('Products/update', views.update_Product_data),
+    path('order/calculate_material', )
 ]
--- diploma/cbApp/views.py
+++ diploma/cbApp/views.py
@@ -1,3 +1,7 @@
+import json
+import uuid
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -11,4 +15,60 @@ def getALLProducts(request):
     srlz = serializer.ProductSerializer(products, many=True)
     return Response(srlz.data)
     #return Response()
+
+@api_view(['POST'])
+def add_product(request):
+    try:
+        data = json.loads(request.body.decode())
+        product_id = uuid.uuid4()
+        cb_models.Products.objects.create(
+           id=product_id,
+           title= data["title"],
+           cost=data["cost"])
+
+        return JsonResponse({
+            'id': product_id
+        })
+    except :
+        return JsonResponse({'error': 'Произошла ошибка'}, status=400)
+
+def get_Product_by_id(request):
+    Product_id = request.GET['id']
+
+    Product = cb_models.Products.objects.get(id=Product_id)
+
+    return JsonResponse({
+        'title': Product.title
+    },status=200)
+
+@api_view(["POST"])
+def update_Product_data(request):
+    try:
+        data = json.loads(request.body.decode())
+        Product = cb_models.Products.objects.get(id=data["id"])
+        Product.title = data["title"]
+        Product.cost = data["cost"]
+        #Product.group_id = data["group_id"]
+        Product.save()
+
+        return JsonResponse({
+            'response': "Данные продукта успешно обновлены"
+        })
+    except :
+        return JsonResponse({'error': 'Произошла ошибка'}, status=400)
+
+@api_view(["POST"])
+def calculate_material(request):
+    try:
+        data = json.loads(request.body.decode())
+        rostovka = cb_models.Rostovka.objects.get(id_product=data["id"],size=data["size"])
+        material_quantity=rostovka.expended_material*data["count"]
+        product=cb_models.Products.objects.get(id=data["id"])
+        material = cb_models.Material.objects.get(id=product.id_material)
+        return JsonResponse({
+            "material_name":material.title,
+            "quantity":material_quantity
+        })
+    except:pass
+
 # Create your views here.

a2b006826974f37f6d44d6d9483865079d569f4d
Эдуард <n3zox1@yandex.ru>
2023-05-12 18:49:27 +0300

    полное решение

--- diploma/cbApp/urls.py
+++ diploma/cbApp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('Products/create', views.add_product),
     re_path(r'Product/$', views.get_Product_by_id),
     path('Products/update', views.update_Product_data),
-    path('order/calculate_material', )
+    path('order/calculate_material', views.calculate_material),
+    path('rostovka/create',views.add_rostovka),
+    path('material/create',views.add_material)
 ]
--- diploma/cbApp/views.py
+++ diploma/cbApp/views.py
@@ -48,7 +48,7 @@ def update_Product_data(request):
         Product = cb_models.Products.objects.get(id=data["id"])
         Product.title = data["title"]
         Product.cost = data["cost"]
-        #Product.group_id = data["group_id"]
+        Product.id_material = data["material_id"]
         Product.save()
 
         return JsonResponse({
@@ -65,10 +65,60 @@ def calculate_material(request):
         material_quantity=rostovka.expended_material*data["count"]
         product=cb_models.Products.objects.get(id=data["id"])
         material = cb_models.Material.objects.get(id=product.id_material)
+
+        is_enougth = material.stock_balance > material_quantity
+
+        if not is_enougth:
+            required = material_quantity - material.stock_balance
+            required_cost = required * material.cost_per_square_meter
+
+            return JsonResponse({
+                "material_name": material.title,
+                "quantity": material_quantity,
+                "is_enougth": is_enougth,
+                "diff": required,
+                "diff_cost": required_cost
+            })
+
         return JsonResponse({
             "material_name":material.title,
-            "quantity":material_quantity
+            "quantity":material_quantity,
+            "is_enougth": is_enougth
         })
     except:pass
+
+@api_view(['POST'])
+def add_rostovka(request):
+    try:
+        data = json.loads(request.body.decode())
+        rostovka_id = uuid.uuid4()
+        cb_models.Rostovka.objects.create(
+            id=rostovka_id,
+            size=data["size"],
+            expended_material=data["expended_material"],
+            id_product= data["product_id"])
+
+        return JsonResponse({
+            'id': rostovka_id
+            })
+    except:
+        return JsonResponse({'error': 'Произошла ошибка'}, status=400)
+
+@api_view(['POST'])
+def add_material(request):
+    try:
+        data = json.loads(request.body.decode())
+        material_id = uuid.uuid4()
+        cb_models.Material.objects.create(
+           id=material_id,
+           title= data["title"],
+           stock_balance=data["stock_balance"],
+           cost_per_square_meter=data["cost_per_square_meter"])
+
+        return JsonResponse({
+            'id': material_id
+        })
+    except :
+        return JsonResponse({'error': 'Произошла ошибка'}, status=400)
 
 # Create your views here.
